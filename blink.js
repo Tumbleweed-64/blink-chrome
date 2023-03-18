0737562e348b974a5bfc1c184f015531efb4cfc3
Tumbleweed-64 <117474637+Tumbleweed-64@users.noreply.github.com>
2023-03-17 17:16:53 -0700

    Create blink.js

---
+++ blink.js
@@ -0,0 +1,9 @@
+function blinkText() {
+    if ((document.body.style.color === "white") || (document.body.style.color === "")) {
+        document.body.style.color = "black";
+    } else if (document.body.style.color === "black") {
+        document.body.style.color = "white";
+    }
+}
+
+window.setInterval(main, 500);
